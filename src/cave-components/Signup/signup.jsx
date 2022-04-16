96d6146bea197c3bad2f83c32301f0a7cb7967a5
Megha-Joshi <megha24joshi@gmail.com>
2022-04-16 19:51:02 +0530

    feat: login-signup-page-added

--- src/cave-components/Signup/signup.jsx
+++ src/cave-components/Signup/signup.jsx
@@ -1,56 +1,37 @@
-import "../../public-css/signup-login.css"
-import "./signup.css";
-import "../../public-css/navbar.css";
 import "../../public-css/root.css";
+import "../../public-css/signup-login.css"
+import { Navbar } from "../Navbar/navbar";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div className="App">
-      <div class="container">
-        <nav class="header justify-align">
-            <div class="left-nav">
-                <a href="/index.html">SPORTS CAVE</a>
-            </div>
-            <div>
-                <input type="text" placeholder="Search" name="search" class="search-box" />
-            </div>
-            <div class="right-nav">
-                <button class="btn-secondary-outline btn btn-text no-margin btn-round"><a href="./login.html" class="login-color">Login</a></button>
-                <div class="badge-item">
-                    <button class="btn btn-only-icon no-margin">
-                        <a href="./wishlist.html" class="items-color"><i class="far fa-heart fa-2x"></i></a></button>
-                    <div class="badge red-circle">9</div>
-                </div>
-                <div class="badge-item">
-                    <button class="btn btn-only-icon no-margin">
-                        <a href="./cart.html" class="items-color"><i class="far fa-shopping-cart fa-2x"></i></a></button>
-                    <div class="badge red-circle">7</div>
-                </div>
-            </div>
-        </nav>
-    </div>
+return (
+<div className="App">
+    <Navbar />
     <div class="login-container justify-align">
         <form class="container form-container">
             <h2 class="login-head">Signup</h2>
-            <label for="username" class="input-text">Email address</label><br>
+            <label for="username" class="input-text">Email address</label><br />
             <input type="text" id="username" name="username" placeholder="abc@gmail.com" class="input-box title-content"
-                required /><br/>
-            <label for="password" class="input-text">Password</label><br/>
+                required /><br />
+            <label for="password" class="input-text">Password</label><br />
             <input type="password" id="password" name="password" placeholder="**********"
                 class="input-box title-content" required />
-                <label for="checkbox">
-                    <input type="checkbox" id="checkbox" name="checkbox" />
-                    I accept all Terms and Conditions</label>
+            <label for="checkbox">
+                <input type="checkbox" id="checkbox" name="checkbox" /> I accept all Terms and Conditions</label>
             <div>
+                <Link to="/">
                 <button class="btn-info btn btn-text long-btn">Create new Account</button>
+                </Link>
             </div>
             <div class="new-ac">
-            <btn class="btn-no-bg"><a href="./login.html" class="btn-no-bg-color">Already have an Account</a></btn>
-        </div>
+                <Link to="/login">
+                <button class="btn-no-bg">Already have an Account</button>
+                </Link>
+            </div>
         </form>
     </div>
-    </div>
-  );
+</div>
+);
 }
 
 export{Signup};
